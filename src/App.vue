906beeba7402232276cc40c66f181a7eb6b77081
Name <zhengxinmiao>
2020-12-31 18:02:22 +0800

    messgae

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <template>
   <div id="app">
     <h1>liweimin</h1>
-    <router-view/>
+    <p>{{ a }}</p>
+    <button @click="test">测试</button>
+    <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  a = 1;
+  b = '2';
+  c = {
+    a: 1,
+    b: '2'
+  };
+
+  test () {
+    console.log(this.a)
+  }
+
+  created () {
+    console.log('app created')
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 </style>
